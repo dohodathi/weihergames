aa8627f18893067a3181bf9d8a11b39f0dbcf97e
Daniel THIEL <daniel.thiel@valeo.com>
2020-04-15 12:01:47 +0200

    fixed path for netlify

--- scripts/weihergames_render_templates.py
+++ scripts/weihergames_render_templates.py
@@ -1,15 +1,17 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 
+root = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(root, '../templates')
 env = Environment(
-    loader=FileSystemLoader('../templates'),
+    loader=FileSystemLoader(templates_dir),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
 
 template = env.get_template('playerprofile.html')
 
-filename = os.path.join('builds', 'player_all.html')
+filename = os.path.join(root, 'builds', 'player_all.html')
 with open(filename, 'w') as fh:
     fh.write(template.render(
         players = [
