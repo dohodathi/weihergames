05ecbcf0e6cae1db560d6ed510a5b4418d2bee70
Daniel THIEL <daniel.thiel@valeo.com>
2020-04-22 10:24:35 +0200

    updated build scripts

--- buildall_netlify.sh
+++ buildall_netlify.sh
@@ -1,5 +1,4 @@
 mkdocs build --verbose -d build
 python scripts/weihergames_elo_calculator.py
-python scripts/weihergames_render_templates.py
-cp scripts/builds/* build/
+python scripts/weihergames_data_extractor.py
 
